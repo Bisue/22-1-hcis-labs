382bc6635cf1402f96ef2f38d659047b6b7f72f2
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-10 18:10:45 +0900

    OpenCV 과제 1

--- OpenCV_1/main.cpp
+++ OpenCV_1/main.cpp
@@ -4,6 +4,11 @@
 
 using namespace cv;
 
+void solve1_addition(const Mat&, const Mat&);
+void solve2_subtraction(const Mat&, const Mat&);
+void solve3_blending(const Mat&, const Mat&, const double&);
+void solve4_adjustment(const Mat&);
+
 CPixel cpixel;
 
 int main()
@@ -13,70 +18,64 @@ int main()
 
 	cpixel = CPixel();
 
-	solve1_1(image1, image2);
+	solve4_adjustment(image1);
 }
 
-void solve1_1(Mat image1, Mat image2)
+void solve1_addition(const Mat& image1, const Mat& image2)
 {
 	if (image1.empty() || image2.empty()) {
 		std::cout << "Image load failed!" << std::endl;
+		return;
 	}
 
 	Mat result = cpixel.GS_add_image(image1, image2);
 
-	imshow("image1", result);
+	imshow("Output Image", result);
 
 	waitKey();
 }
 
-void solve1_2(Mat image1, Mat image2)
+void solve2_subtraction(const Mat& image1, const Mat& image2)
 {
 	if (image1.empty() || image2.empty()) {
 		std::cout << "Image load failed!" << std::endl;
-	}
-
-	Mat result = cpixel.GS_add_image(image1, image2);
-
-	imshow("image1", result);
-
-	waitKey();
-}
-
-void solve2_1(Mat image1, Mat image2)
-{
-	if (image1.empty() || image2.empty()) {
-		std::cout << "Image load failed!" << std::endl;
+		return;
 	}
 
 	Mat result = cpixel.GS_subtract_image(image1, image2);
 
-	imshow("image1", result);
+	imshow("Output Image", result);
 
 	waitKey();
 }
 
-void solve2_2(Mat image1, Mat image2)
+void solve3_blending(const Mat& image1, const Mat& image2, const double& alpha)
 {
 	if (image1.empty() || image2.empty()) {
 		std::cout << "Image load failed!" << std::endl;
+		return;
 	}
 
-	Mat result = cpixel.GS_subtract_image(image1, image2);
+	Mat result = cpixel.GS_blending_effect(image1, image2, alpha);
 
-	imshow("image1", result);
+	imshow("Output Image", result);
 
 	waitKey();
 }
 
-void solve3_1(Mat image1, Mat image2)
+void solve4_adjustment(const Mat& image)
 {
-	if (image1.empty() || image2.empty()) {
+	if (image.empty()) {
 		std::cout << "Image load failed!" << std::endl;
+		return;
 	}
 
-	Mat result = cpixel.GS_blending_effect(image1, image2, 0);
+	Mat grayScaled;
+	cvtColor(image, grayScaled, COLOR_RGB2GRAY);
 
-	imshow("image1", result);
+	Mat result = cpixel.GS_LUT_basic_contrast_brightness(grayScaled, 2, 50);
+
+	imshow("Output Image", result);
 
 	waitKey();
 }
