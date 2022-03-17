4c8ec9dcad487b44a50cc35933bf6b88392c4eff
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-17 14:10:02 +0900

    실습2 1-1

--- OpenCV_2/main.cpp
+++ OpenCV_2/main.cpp
@@ -29,15 +29,21 @@ void solve(const Mat& image1, const Mat& image2)
 {
 	// 두 개의 이미지를 각각 컬러 이미지를 흑백 이미지로 변경
 	Mat grayScaled1;
-	cvtColor(image1, grayScaled1, COLOR_RGB2GRAY);
+	cvtColor(image1, grayScaled1, COLOR_BGR2GRAY);
 	Mat grayScaled2;
-	cvtColor(image2, grayScaled2, COLOR_RGB2GRAY);
+	cvtColor(image2, grayScaled2, COLOR_BGR2GRAY);
+
+	imwrite("./outputs/1_1_input_1.png", grayScaled1);
+	imwrite("./outputs/1_1_input_2.png", grayScaled2);
 
 	// 각각의 흑백 이미지에 대해서 histogram 생성
 	Mat hist1 = cpixel.GS_imhist(grayScaled1);
 	imshow("Histogram 1", hist1);
 	Mat hist2 = cpixel.GS_imhist(grayScaled2);
 	imshow("Histogram 2", hist2);
+
+	imwrite("./outputs/1_1_output_1.png", grayScaled1);
+	imwrite("./outputs/1_1_output_2.png", grayScaled2);
 
 	waitKey();
 }
