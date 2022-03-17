f645d7fe0a478d00a1dda742c3f8c8122462efef
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-17 14:15:00 +0900

    실습2 2-1

--- OpenCV_2/main.cpp
+++ OpenCV_2/main.cpp
@@ -4,8 +4,8 @@
 
 using namespace cv;
 
-// 문제 1-3
-void solve(const Mat&, const Mat&);
+// 문제 2-1
+void solve(const Mat&);
 
 // 모든 함수에서 편하게 쓰도록 전역 CPixel 인스턴스 선언
 CPixel cpixel;
@@ -15,38 +15,27 @@ int main()
 	// 연산 대상 이미지 로드
 	// (이미지들은 <프로젝트경로/images/> 에 들어있는 것으로 가정)
 	Mat image1 = imread("./images/lenna.png");
-	Mat image2 = imread("./images/stuff_color_1.png");
 
 	// cpixel 인스턴스 생성
 	cpixel = CPixel();
 
 	// 문제별 함수 분리
-	solve(image1, image2);
+	solve(image1);
 }
 
-// 문제 1-3
-void solve(const Mat& image1, const Mat& image2)
+// 문제 2-1
+void solve(const Mat& image)
 {
+	imwrite("./outputs/2_1_input.png", image);
+
 	// 두 개의 이미지를 각각 컬러 이미지를 흑백 이미지로 변경
-	Mat grayScaled1;
-	cvtColor(image1, grayScaled1, COLOR_BGR2GRAY);
-	Mat grayScaled2;
-	cvtColor(image2, grayScaled2, COLOR_BGR2GRAY);
+	Mat grayScaled;
+	cvtColor(image, grayScaled, COLOR_RGB2GRAY);
 
-	imwrite("./outputs/1_3_input_1.png", grayScaled1);
-	imwrite("./outputs/1_3_input_2.png", grayScaled2);
+	// 흑백 이미지 출력
+	imshow("Gray Scaled Image", grayScaled);
 
-	// 각각의 흑백 이미지에 대해서 histogram equalization 실행 후, 결과 이미지에 대해서 histogram 이미지 생성
-	Mat equal1 = cpixel.GS_histeq(grayScaled1);
-	Mat hist1 = cpixel.GS_imhist(equal1);
-	imshow("A_equlizedHist", hist1);
-
-	Mat equal2 = cpixel.GS_histeq(grayScaled2);
-	Mat hist2 = cpixel.GS_imhist(equal2);
-	imshow("B_equlizedHist", hist2);
-
-	imwrite("./outputs/1_3_output_1.png", hist1);
-	imwrite("./outputs/1_3_output_2.png", hist2);
+	imwrite("./outputs/2_1_output.png", grayScaled);
 
 	waitKey();
 }
