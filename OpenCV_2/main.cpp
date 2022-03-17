ca015973d80fb538db6e1360a7699ff0ab292e18
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-17 14:20:45 +0900

    실습2 2-3

--- OpenCV_2/main.cpp
+++ OpenCV_2/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace cv;
 
-// 문제 2-2
+// 문제 2-3
 void solve(const Mat&);
 
 // 모든 함수에서 편하게 쓰도록 전역 CPixel 인스턴스 선언
@@ -23,20 +23,20 @@ int main()
 	solve(image1);
 }
 
-// 문제 2-2
+// 문제 2-3
 void solve(const Mat& image)
 {
 	// 두 개의 이미지를 각각 컬러 이미지를 흑백 이미지로 변경
 	Mat grayScaled;
 	cvtColor(image, grayScaled, COLOR_BGR2GRAY);
 
-	imwrite("./outputs/2_2_input.png", grayScaled);
+	imwrite("./outputs/2_3_input.png", grayScaled);
 
-	// GS_threshold 메서드를 이용하여 임계값에 대해 이미지 이진화
-	Mat binaryImage = cpixel.GS_threshold(grayScaled, 150, THRESH_BINARY);
+	// GS_threshold 메서드를 이용하여 임계값에 대해 OTSU 방식으로 이미지 이진화
+	Mat binaryImage = cpixel.GS_threshold(grayScaled, 0, THRESH_BINARY | THRESH_OTSU);
 	imshow("Binary Image", binaryImage);
 
-	imwrite("./outputs/2_2_output.png", binaryImage);
+	imwrite("./outputs/2_3_output.png", binaryImage);
 
 	waitKey();
 }
