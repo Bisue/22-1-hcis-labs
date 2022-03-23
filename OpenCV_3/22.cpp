4f6493022d199cb76ea2fc24b572fa85aa424c4e
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-23 23:48:24 +0900

    2-3, 2-3 output

--- OpenCV_3/22.cpp
+++ OpenCV_3/22.cpp
@@ -1,38 +1,38 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Color.h"
-
-using namespace cv;
-
-int main()
-{
-	// input 이미지 로드 (이전에 만든 이미지 사용)
-	// (흑백 이미지로 명시)
-	Mat hsv = imread("./outputs/2_1_output.png");
-	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
-	imwrite("./outputs/2_2_input.png", hsv);
-
-	// =======================================================
-
-	// hsv 이미지를 h, s, v로 split
-	Mat channels[3];
-	split(hsv, channels);
-
-	Mat h = channels[0];
-	Mat s = channels[1];
-	Mat v = channels[2];
-
-	// =======================================================
-
-	// output 이미지 디스플레이
-	imshow("H", h);
-	imshow("S", s);
-	imshow("V", v);
-
-	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
-	imwrite("./outputs/2_2_output_1.png", h);
-	imwrite("./outputs/2_2_output_2.png", s);
-	imwrite("./outputs/2_2_output_3.png", v);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Color.h"
+//
+//using namespace cv;
+//
+//int main()
+//{
+//	// input 이미지 로드 (이전에 만든 이미지 사용)
+//	// (흑백 이미지로 명시)
+//	Mat hsv = imread("./outputs/2_1_output.png");
+//	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
+//	imwrite("./outputs/2_2_input.png", hsv);
+//
+//	// =======================================================
+//
+//	// hsv 이미지를 h, s, v로 split
+//	Mat channels[3];
+//	split(hsv, channels);
+//
+//	Mat h = channels[0];
+//	Mat s = channels[1];
+//	Mat v = channels[2];
+//
+//	// =======================================================
+//
+//	// output 이미지 디스플레이
+//	imshow("H", h);
+//	imshow("S", s);
+//	imshow("V", v);
+//
+//	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
+//	imwrite("./outputs/2_2_output_1.png", h);
+//	imwrite("./outputs/2_2_output_2.png", s);
+//	imwrite("./outputs/2_2_output_3.png", v);
+//
+//	waitKey();
+//}
---
+++ OpenCV_3/23.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Pixel.h"
+
+using namespace cv;
+
+int main()
+{
+	// input 이미지 로드 (이전에 만든 이미지 사용)
+	// (흑백 이미지로 명시)
+	Mat v = imread("./outputs/2_2_output_3.png", 0);
+	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
+	imwrite("./outputs/2_3_input.png", v);
+
+	// =======================================================
+
+	CPixel cp;
+
+	// hsv 중 v 채널에 대해서 equalization 실행
+	Mat equalizedV = cp.GS_histeq(v);
+
+	// =======================================================
+
+	// output 이미지 디스플레이
+	imshow("Equalized V", equalizedV);
+
+	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
+	imwrite("./outputs/2_3_output.png", equalizedV);
+
+	waitKey();
+}
