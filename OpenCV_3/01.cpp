5c48e5c7090d2ec61a93afe93c3618687be13d57
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-23 22:03:45 +0900

    0-2, 0-2 output

--- OpenCV_3/01.cpp
+++ OpenCV_3/01.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-
-using namespace cv;
-
-// 이미지의 흑백 이미지 반환
-Mat makeGrayScaled(const Mat& image) {
-	Mat gray;
-	// cv::cvtColor를 이용해 BGR에서 GRAY이미지로 변환
-	cvtColor(image, gray, COLOR_BGR2GRAY);
-
-	return gray;
-}
-
-int main()
-{
-	// input 이미지 로드 (./images 디렉토리에 원본 컬러 이미지들이 있다고 가정)
-	Mat originLenna = imread("./images/lenna.png");
-	Mat originJenny = imread("./images/jenny.jpg");
-	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
-	imwrite("./outputs/0_1_input_1.png", originLenna);
-	imwrite("./outputs/0_1_input_2.png", originJenny);
-
-	// =======================================================
-
-	// 이미지 각각 흑백 변환
-	Mat grayLenna = makeGrayScaled(originLenna);
-	Mat grayJenny = makeGrayScaled(originJenny);
-
-	// =======================================================
-
-	// output 이미지 디스플레이
-	imshow("Lenna", grayLenna);
-	imshow("Jenny", grayJenny);
-	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
-	imwrite("./outputs/0_1_output_1.png", grayLenna);
-	imwrite("./outputs/0_1_output_2.png", grayJenny);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//
+//using namespace cv;
+//
+//// 이미지의 흑백 이미지 반환
+//Mat makeGrayScaled(const Mat& image) {
+//	Mat gray;
+//	// cv::cvtColor를 이용해 BGR에서 GRAY이미지로 변환
+//	cvtColor(image, gray, COLOR_BGR2GRAY);
+//
+//	return gray;
+//}
+//
+//int main()
+//{
+//	// input 이미지 로드 (./images 디렉토리에 원본 컬러 이미지들이 있다고 가정)
+//	Mat originLenna = imread("./images/lenna.png");
+//	Mat originJenny = imread("./images/jenny.jpg");
+//	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
+//	imwrite("./outputs/0_1_input_1.png", originLenna);
+//	imwrite("./outputs/0_1_input_2.png", originJenny);
+//
+//	// =======================================================
+//
+//	// 이미지 각각 흑백 변환
+//	Mat grayLenna = makeGrayScaled(originLenna);
+//	Mat grayJenny = makeGrayScaled(originJenny);
+//
+//	// =======================================================
+//
+//	// output 이미지 디스플레이
+//	imshow("Lenna", grayLenna);
+//	imshow("Jenny", grayJenny);
+//	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
+//	imwrite("./outputs/0_1_output_1.png", grayLenna);
+//	imwrite("./outputs/0_1_output_2.png", grayJenny);
+//
+//	waitKey();
+//}
---
+++ OpenCV_3/02.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Filter.h"
+
+using namespace cv;
+
+// blurred 된 이미지를 만들어 반환
+Mat makeBlurred(const Mat& image)
+{
+	CFilter cf;
+
+	// CFilter의 blurring 메서드 사용
+	// (option 1 - OpenCV 버전)
+	return cf.GS_blurring(image, 1, CV_BLUR);
+}
+
+int main()
+{
+	// input 이미지 로드 (0-1에서 만든 grayscaled 이미지 사용)
+	Mat grayLenna = imread("./outputs/0_1_output_1.png");
+	Mat grayJenny = imread("./outputs/0_1_output_2.png");
+	// input 이미지 저장 (./outputs 디렉토리가 있다고 가정)
+	imwrite("./outputs/0_2_input_1.png", grayLenna);
+	imwrite("./outputs/0_2_input_2.png", grayJenny);
+
+	// =======================================================
+
+	// 이미지 각각 흑백 변환
+	Mat blurredLenna = makeBlurred(grayLenna);
+	Mat blurredJenny = makeBlurred(grayJenny);
+
+	// =======================================================
+
+	// output 이미지 디스플레이
+	imshow("Lenna", blurredLenna);
+	imshow("Jenny", blurredJenny);
+	// output 이미지 저장 (./output 디렉토리가 있다고 가정)
+	imwrite("./outputs/0_2_output_1.png", blurredLenna);
+	imwrite("./outputs/0_2_output_2.png", blurredJenny);
+
+	waitKey();
+}
