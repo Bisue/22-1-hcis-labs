3a154d897221e60443238801c82327ba98face4a
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-04-01 00:58:59 +0900

    3-3 주석

--- OpenCV_4/33.cpp
+++ OpenCV_4/33.cpp
@@ -1,26 +1,34 @@
-//#include <iostream>
-//#include <opencv2/opencv.hpp>
-//#include "Pixel.h"
-//
-//using namespace cv;
-//
-//int main()
-//{
-//	Mat imageA = imread("./images/briefcase.jpg");
-//	Mat imageB = imread("./outputs/3_2_output.png");
-//	imwrite("./outputs/3_3_input_1.png", imageA);
-//	imwrite("./outputs/3_3_input_2.png", imageB);
-//
-//	// =======================================================
-//
-//	CPixel cp;
-//
-//	Mat subtracted = cp.GS_subtract_image(imageA, imageB);
-//
-//	// =======================================================
-//
-//	imshow("Subtract Result Briefcase", subtracted);
-//	imwrite("./outputs/3_3_output.png", subtracted);
-//
-//	waitKey();
-//}
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Pixel.h"
+
+using namespace cv;
+
+int main()
+{
+	// 입력 이미지 로드
+	// (컬러 이미지)
+	Mat imageA = imread("./images/briefcase.jpg");
+	Mat imageB = imread("./outputs/3_2_output.png");
+	// 입력 이미지 파일로 저장
+	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정
+	imwrite("./outputs/3_3_input_1.png", imageA);
+	imwrite("./outputs/3_3_input_2.png", imageB);
+
+	// =======================================================
+
+	CPixel cp;
+
+	// 이미지 A(원본 briefcase) 에서 이미지 B(360 rotated of briefcase) 를 뺌
+	Mat subtracted = cp.GS_subtract_image(imageA, imageB);
+
+	// =======================================================
+
+	// 출력 이미지 확인
+	imshow("Subtract Result Briefcase", subtracted);
+	// 출력 이미지 파일로 저장
+	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
+	imwrite("./outputs/3_3_output.png", subtracted);
+
+	waitKey();
+}
