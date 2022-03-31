1cb3a9de51f814750510494ab517dbc31500ee80
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-31 14:02:32 +0900

    2-5

--- OpenCV_4/24.cpp
+++ OpenCV_4/24.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Geometry.h"
-
-using namespace cv;
-
-Mat rotate(const Mat& image, const int degree)
-{
-	CGeometry cg;
-
-	Mat rotated = cg.GS_affineRotate(image, degree, 1);
-
-	return rotated;
-}
-
-int main()
-{
-	Mat originBriefcase = imread("./outputs/2_3_output.png");
-	imwrite("./outputs/2_4_input.png", originBriefcase);
-
-	// =======================================================
-
-	Mat rotated = rotate(originBriefcase, -45);
-
-	// =======================================================
-
-	imshow("Rotated of Scaled Briefcase", rotated);
-	imwrite("./outputs/2_4_output.png", rotated);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Geometry.h"
+//
+//using namespace cv;
+//
+//Mat rotate(const Mat& image, const int degree)
+//{
+//	CGeometry cg;
+//
+//	Mat rotated = cg.GS_affineRotate(image, degree, 1);
+//
+//	return rotated;
+//}
+//
+//int main()
+//{
+//	Mat originBriefcase = imread("./outputs/2_3_output.png");
+//	imwrite("./outputs/2_4_input.png", originBriefcase);
+//
+//	// =======================================================
+//
+//	Mat rotated = rotate(originBriefcase, -45);
+//
+//	// =======================================================
+//
+//	imshow("Rotated of Scaled Briefcase", rotated);
+//	imwrite("./outputs/2_4_output.png", rotated);
+//
+//	waitKey();
+//}
