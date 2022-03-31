2ced75e02c32e38b05cd954bd3680c1cb68e3075
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-04-01 00:50:03 +0900

    2-4 주석

--- OpenCV_4/23.cpp
+++ OpenCV_4/23.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Geometry.h"
-
-using namespace cv;
-
-// 입력 이미지를 amount만큼 scale(확대 or 축소) 시킴
-Mat scale(const Mat& image, const double amount)
-{
-	CGeometry cg;
-
-	// CGeometry의 GS_affineRotate 메서드를 사용해
-	// 이미지를 affine 변환시킴.
-	// (이때, 회전은 시키지 않고 확대/축소만 하기 위하여 angle은 0으로 할당)
-	Mat rotated = cg.GS_affineRotate(image, 0, amount);
-
-	return rotated;
-}
-
-int main()
-{
-	// 원본 이미지 로드
-	// (프로젝트폴더/images에 원본 이미지들이 있다고 가정)
-	Mat originBriefcase = imread("./images/briefcase.jpg");
-	// 입력 이미지 파일로 저장
-	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
-	imwrite("./outputs/2_3_input.png", originBriefcase);
-
-	// =======================================================
-
-	// 입력 이미지를 1.5배 확대시킴
-	Mat scaled = scale(originBriefcase, 1.5);
-
-	// =======================================================
-
-	// 출력 이미지 확인
-	imshow("Scaled Briefcase", scaled);
-	// 출력 이미지 파일로 저장
-	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
-	imwrite("./outputs/2_3_output.png", scaled);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Geometry.h"
+//
+//using namespace cv;
+//
+//// 입력 이미지를 amount만큼 scale(확대 or 축소) 시킴
+//Mat scale(const Mat& image, const double amount)
+//{
+//	CGeometry cg;
+//
+//	// CGeometry의 GS_affineRotate 메서드를 사용해
+//	// 이미지를 affine 변환시킴.
+//	// (이때, 회전은 시키지 않고 확대/축소만 하기 위하여 angle은 0으로 할당)
+//	Mat rotated = cg.GS_affineRotate(image, 0, amount);
+//
+//	return rotated;
+//}
+//
+//int main()
+//{
+//	// 원본 이미지 로드
+//	// (프로젝트폴더/images에 원본 이미지들이 있다고 가정)
+//	Mat originBriefcase = imread("./images/briefcase.jpg");
+//	// 입력 이미지 파일로 저장
+//	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
+//	imwrite("./outputs/2_3_input.png", originBriefcase);
+//
+//	// =======================================================
+//
+//	// 입력 이미지를 1.5배 확대시킴
+//	Mat scaled = scale(originBriefcase, 1.5);
+//
+//	// =======================================================
+//
+//	// 출력 이미지 확인
+//	imshow("Scaled Briefcase", scaled);
+//	// 출력 이미지 파일로 저장
+//	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
+//	imwrite("./outputs/2_3_output.png", scaled);
+//
+//	waitKey();
+//}
--- OpenCV_4/24.cpp
+++ OpenCV_4/24.cpp
@@ -1,31 +1,43 @@
-//#include <iostream>
-//#include <opencv2/opencv.hpp>
-//#include "Geometry.h"
-//
-//using namespace cv;
-//
-//Mat rotate(const Mat& image, const int degree)
-//{
-//	CGeometry cg;
-//
-//	Mat rotated = cg.GS_affineRotate(image, degree, 1);
-//
-//	return rotated;
-//}
-//
-//int main()
-//{
-//	Mat originBriefcase = imread("./outputs/2_3_output.png");
-//	imwrite("./outputs/2_4_input.png", originBriefcase);
-//
-//	// =======================================================
-//
-//	Mat rotated = rotate(originBriefcase, -45);
-//
-//	// =======================================================
-//
-//	imshow("Rotated of Scaled Briefcase", rotated);
-//	imwrite("./outputs/2_4_output.png", rotated);
-//
-//	waitKey();
-//}
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Geometry.h"
+
+using namespace cv;
+
+// 입력 이미지를 해당 degree 만큼 반시계방향으로 회전
+Mat rotate(const Mat& image, const int degree)
+{
+	CGeometry cg;
+
+	// CGeometry의 GS_affineRotate 함수를 사용하여
+	// 해당 이미지를 affine 변환시킴.
+	// (이때, 회전만 적용하기 위하여 scale을 1로 함)
+	Mat rotated = cg.GS_affineRotate(image, degree, 1);
+
+	return rotated;
+}
+
+int main()
+{
+	// 입력 이미지 로드
+	// (컬러 이미지)
+	Mat originBriefcase = imread("./outputs/2_3_output.png");
+	// 입력 이미지 파일로 저장
+	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
+	imwrite("./outputs/2_4_input.png", originBriefcase);
+
+	// =======================================================
+
+	// 이미지를 시계방향(clockwise)으로 45도 회전시킴
+	Mat rotated = rotate(originBriefcase, -45);
+
+	// =======================================================
+
+	// 출력 이미지 확인
+	imshow("Rotated of Scaled Briefcase", rotated);
+	// 출력 이미지 파일로 저장
+	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
+	imwrite("./outputs/2_4_output.png", rotated);
+
+	waitKey();
+}
