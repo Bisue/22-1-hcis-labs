261e897508f69dceedf915501bbe50ab42840554
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-31 13:23:44 +0900

    1-3

--- OpenCV_4/12.cpp
+++ OpenCV_4/12.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Edge.h"
-
-using namespace cv;
-
-Mat getEdgeSobel(const Mat& image)
-{
-	CEdge ce;
-
-	Mat edge = ce.GS_sobel_edge_Sobel(image, 2);
-
-	return edge;
-}
-
-int main()
-{
-	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
-	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
-	imwrite("./outputs/1_2_input_1.png", grayJenny);
-	imwrite("./outputs/1_2_input_2.png", grayRice);
-
-	// =======================================================
-
-	Mat edgeJenny = getEdgeSobel(grayJenny);
-	Mat edgeRice = getEdgeSobel(grayRice);
-
-	// =======================================================
-
-	imshow("Sobel Edge of Jenny", edgeJenny);
-	imshow("Sobel Edge of Rice", edgeRice);
-	imwrite("./outputs/1_2_output_1.png", edgeJenny);
-	imwrite("./outputs/1_2_output_2.png", edgeRice);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Edge.h"
+//
+//using namespace cv;
+//
+//Mat getEdgeSobel(const Mat& image)
+//{
+//	CEdge ce;
+//
+//	Mat edge = ce.GS_sobel_edge_Sobel(image, 2);
+//
+//	return edge;
+//}
+//
+//int main()
+//{
+//	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
+//	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
+//	imwrite("./outputs/1_2_input_1.png", grayJenny);
+//	imwrite("./outputs/1_2_input_2.png", grayRice);
+//
+//	// =======================================================
+//
+//	Mat edgeJenny = getEdgeSobel(grayJenny);
+//	Mat edgeRice = getEdgeSobel(grayRice);
+//
+//	// =======================================================
+//
+//	imshow("Sobel Edge of Jenny", edgeJenny);
+//	imshow("Sobel Edge of Rice", edgeRice);
+//	imwrite("./outputs/1_2_output_1.png", edgeJenny);
+//	imwrite("./outputs/1_2_output_2.png", edgeRice);
+//
+//	waitKey();
+//}
---
+++ OpenCV_4/13.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Edge.h"
+
+using namespace cv;
+
+Mat getEdgeLaplacian(const Mat& image)
+{
+	CEdge ce;
+
+	Mat edge = ce.GS_laplacian_edge_Laplacian(image);
+
+	return edge;
+}
+
+int main()
+{
+	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
+	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
+	imwrite("./outputs/1_3_input_1.png", grayJenny);
+	imwrite("./outputs/1_3_input_2.png", grayRice);
+
+	// =======================================================
+
+	Mat edgeJenny = getEdgeLaplacian(grayJenny);
+	Mat edgeRice = getEdgeLaplacian(grayRice);
+
+	// =======================================================
+
+	imshow("Laplacian Edge of Jenny", edgeJenny);
+	imshow("Laplacian Edge of Rice", edgeRice);
+	imwrite("./outputs/1_3_output_1.png", edgeJenny);
+	imwrite("./outputs/1_3_output_2.png", edgeRice);
+
+	waitKey();
+}
