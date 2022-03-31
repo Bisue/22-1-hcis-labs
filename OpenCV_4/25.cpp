96d05d7453d2700106f5f27f071524ab461dac1d
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-31 14:03:47 +0900

    3-1

--- OpenCV_4/25.cpp
+++ OpenCV_4/25.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Pixel.h"
-
-using namespace cv;
-
-int main()
-{
-	Mat imageA = imread("./outputs/2_2_output.png");
-	Mat imageB = imread("./outputs/2_4_output.png");
-	imwrite("./outputs/2_5_input_1.png", imageA);
-	imwrite("./outputs/2_5_input_2.png", imageB);
-
-	// =======================================================
-
-	CPixel cp;
-
-	Mat subtracted = cp.GS_subtract_image(imageA, imageB);
-
-	// =======================================================
-
-	imshow("Subtract Result Briefcase", subtracted);
-	imwrite("./outputs/2_5_output.png", subtracted);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Pixel.h"
+//
+//using namespace cv;
+//
+//int main()
+//{
+//	Mat imageA = imread("./outputs/2_2_output.png");
+//	Mat imageB = imread("./outputs/2_4_output.png");
+//	imwrite("./outputs/2_5_input_1.png", imageA);
+//	imwrite("./outputs/2_5_input_2.png", imageB);
+//
+//	// =======================================================
+//
+//	CPixel cp;
+//
+//	Mat subtracted = cp.GS_subtract_image(imageA, imageB);
+//
+//	// =======================================================
+//
+//	imshow("Subtract Result Briefcase", subtracted);
+//	imwrite("./outputs/2_5_output.png", subtracted);
+//
+//	waitKey();
+//}
