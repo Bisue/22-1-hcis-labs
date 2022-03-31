a857cbe7bfb76ec090657d681cf0e11c2d820cf4
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-31 14:10:20 +0900

    3-3

--- OpenCV_4/32.cpp
+++ OpenCV_4/32.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Geometry.h"
-
-using namespace cv;
-
-Mat rotate(const Mat& image, const int degree)
-{
-	CGeometry cg;
-
-	Mat rotated = cg.GS_affineRotate(image, degree, 1);
-
-	return rotated;
-}
-
-int main()
-{
-	Mat rotatedBriefcase = imread("./outputs/3_1_output.png");
-	imwrite("./outputs/3_2_input.png", rotatedBriefcase);
-
-	// =======================================================
-
-	for (int i = 0; i < 11; i++) {
-		rotatedBriefcase = rotate(rotatedBriefcase, -30);
-	}
-
-	// =======================================================
-
-	imshow("Rotated Briefcase", rotatedBriefcase);
-	imwrite("./outputs/3_2_output.png", rotatedBriefcase);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Geometry.h"
+//
+//using namespace cv;
+//
+//Mat rotate(const Mat& image, const int degree)
+//{
+//	CGeometry cg;
+//
+//	Mat rotated = cg.GS_affineRotate(image, degree, 1);
+//
+//	return rotated;
+//}
+//
+//int main()
+//{
+//	Mat rotatedBriefcase = imread("./outputs/3_1_output.png");
+//	imwrite("./outputs/3_2_input.png", rotatedBriefcase);
+//
+//	// =======================================================
+//
+//	for (int i = 0; i < 11; i++) {
+//		rotatedBriefcase = rotate(rotatedBriefcase, -30);
+//	}
+//
+//	// =======================================================
+//
+//	imshow("Rotated Briefcase", rotatedBriefcase);
+//	imwrite("./outputs/3_2_output.png", rotatedBriefcase);
+//
+//	waitKey();
+//}
---
+++ OpenCV_4/33.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Pixel.h"
+
+using namespace cv;
+
+int main()
+{
+	Mat imageA = imread("./images/briefcase.jpg");
+	Mat imageB = imread("./outputs/3_2_output.png");
+	imwrite("./outputs/3_3_input_1.png", imageA);
+	imwrite("./outputs/3_3_input_2.png", imageB);
+
+	// =======================================================
+
+	CPixel cp;
+
+	Mat subtracted = cp.GS_subtract_image(imageA, imageB);
+
+	// =======================================================
+
+	imshow("Subtract Result Briefcase", subtracted);
+	imwrite("./outputs/3_3_output.png", subtracted);
+
+	waitKey();
+}
