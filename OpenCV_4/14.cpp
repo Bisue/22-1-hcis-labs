423b0aeca25f53738d5b5917aeb64306158f66e5
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-03-31 13:33:21 +0900

    1-5 (3x3, 5x5 이상은 거의 안나옴)

--- OpenCV_4/14.cpp
+++ OpenCV_4/14.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include "Edge.h"
-
-using namespace cv;
-
-Mat getEdgeCanny(const Mat& image)
-{
-	CEdge ce;
-
-	Mat edge = ce.GS_canny_edge_Canny(image);
-
-	return edge;
-}
-
-int main()
-{
-	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
-	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
-	imwrite("./outputs/1_4_input_1.png", grayJenny);
-	imwrite("./outputs/1_4_input_2.png", grayRice);
-
-	// =======================================================
-
-	Mat edgeJenny = getEdgeCanny(grayJenny);
-	Mat edgeRice = getEdgeCanny(grayRice);
-
-	// =======================================================
-
-	imshow("Canny Edge of Jenny", edgeJenny);
-	imshow("Canny Edge of Rice", edgeRice);
-	imwrite("./outputs/1_4_output_1.png", edgeJenny);
-	imwrite("./outputs/1_4_output_2.png", edgeRice);
-
-	waitKey();
-}
+//#include <iostream>
+//#include <opencv2/opencv.hpp>
+//#include "Edge.h"
+//
+//using namespace cv;
+//
+//Mat getEdgeCanny(const Mat& image)
+//{
+//	CEdge ce;
+//
+//	Mat edge = ce.GS_canny_edge_Canny(image);
+//
+//	return edge;
+//}
+//
+//int main()
+//{
+//	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
+//	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
+//	imwrite("./outputs/1_4_input_1.png", grayJenny);
+//	imwrite("./outputs/1_4_input_2.png", grayRice);
+//
+//	// =======================================================
+//
+//	Mat edgeJenny = getEdgeCanny(grayJenny);
+//	Mat edgeRice = getEdgeCanny(grayRice);
+//
+//	// =======================================================
+//
+//	imshow("Canny Edge of Jenny", edgeJenny);
+//	imshow("Canny Edge of Rice", edgeRice);
+//	imwrite("./outputs/1_4_output_1.png", edgeJenny);
+//	imwrite("./outputs/1_4_output_2.png", edgeRice);
+//
+//	waitKey();
+//}
---
+++ OpenCV_4/15.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Edge.h"
+#include "Filter.h"
+
+using namespace cv;
+
+Mat getLaplacianOfGaussian(const Mat& image)
+{
+	CEdge ce;
+	CFilter cf;
+
+	Mat gaussian = cf.GS_blurring(image, CV_GAUSSIAN, 0);
+
+	Mat laplacian = ce.GS_laplacian_edge_Laplacian(gaussian);
+
+	return laplacian;
+}
+
+int main()
+{
+	Mat grayJenny = imread("./outputs/1_1_output_1.png", 0);
+	Mat grayRice = imread("./outputs/1_1_output_2.png", 0);
+	imwrite("./outputs/1_5_input_1.png", grayJenny);
+	imwrite("./outputs/1_5_input_2.png", grayRice);
+
+	// =======================================================
+
+	Mat lofgJenny = getLaplacianOfGaussian(grayJenny);
+	Mat lofgRice = getLaplacianOfGaussian(grayRice);
+
+	// =======================================================
+
+	imshow("Laplacian of Gaussian of Jenny", lofgJenny);
+	imshow("Laplacian of Gaussian of Rice", lofgRice);
+	imwrite("./outputs/1_5_output_1.png", lofgJenny);
+	imwrite("./outputs/1_5_output_2.png", lofgRice);
+
+	waitKey();
+}
