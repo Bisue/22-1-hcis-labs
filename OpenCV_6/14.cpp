b3c5a8825445fb9e5746c1d91ab3d6036b5dc6b5
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-04-13 20:24:56 +0900

    변수명 edge에서 subtraction으로 표현 수정

--- OpenCV_6/14.cpp
+++ OpenCV_6/14.cpp
@@ -7,20 +7,20 @@
 //int main()
 //{
 //	// 입력 이미지 로드
-//	Mat edgeStuff = imread("./outputs/1_3_output_1.png", 0);
-//	Mat edgeRice = imread("./outputs/1_3_output_2.png", 0);
+//	Mat subtractionStuff = imread("./outputs/1_3_output_1.png", 0);
+//	Mat subtractionRice = imread("./outputs/1_3_output_2.png", 0);
 //	// 입력 이미지 파일로 저장
 //	// (프로젝트폴더/outputs 폴더가 이미 있다고 가정)
-//	imwrite("./outputs/1_4_input_1.png", edgeStuff);
-//	imwrite("./outputs/1_4_input_2.png", edgeRice);
+//	imwrite("./outputs/1_4_input_1.png", subtractionStuff);
+//	imwrite("./outputs/1_4_input_2.png", subtractionRice);
 //
 //	// =======================================================
 //
 //	CPixel cp;
 //
 //	// 4. 1-3에서 빼기로 추출된 이미지에 otzu binarization 실행
-//	Mat binaryStuff = cp.GS_threshold(edgeStuff, 0, THRESH_BINARY | THRESH_OTSU);
-//	Mat binaryRice = cp.GS_threshold(edgeRice, 0, THRESH_BINARY | THRESH_OTSU);
+//	Mat binaryStuff = cp.GS_threshold(subtractionStuff, 0, THRESH_BINARY | THRESH_OTSU);
+//	Mat binaryRice = cp.GS_threshold(subtractionRice, 0, THRESH_BINARY | THRESH_OTSU);
 //
 //	// =======================================================
 //
