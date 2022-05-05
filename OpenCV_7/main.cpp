8781a57ed3306a50db4f9d0b3447f9ce602bc1b6
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-05-05 19:45:59 +0900

    실습7

--- OpenCV_7/main.cpp
+++ OpenCV_7/main.cpp
@@ -35,28 +35,36 @@ int main()
 
 	VideoCapture video(0);
 
-	Mat cameraRaw;
-	Mat cameraResized;
-	Mat masked;
+
 	while (true) {
-		video >> cameraRaw;
+		// Step 1.
+		Mat camera;
+		Mat masked;
+		video >> camera;
 
-		resize(cameraRaw, cameraResized, Size(600, 400));
-		add(cameraResized, mask, masked);
+		resize(mask, mask, Size(camera.size().width, camera.size().height));
+		add(camera, mask, masked);
 
-		// ==
+		imshow("Step 1.", camera);
 
+		// Step 2.
 		Mat converted;
 		cvtColor(masked, converted, COLOR_BGR2YCrCb);
 
 		Mat skin;
 		inRange(converted, Scalar(0, 130, 90), Scalar(255, 168, 130), skin);
 
+		imshow("Step 2.", skin);
+
+		// Step 3.
 		Mat grayMasked;
 		cvtColor(masked, grayMasked, COLOR_BGR2GRAY);
 
 		Mat skinGray = grayMasked + (~skin);
 
+		imshow("Step 3.", skinGray);
+
+		// Step 4.
 		Mat binary;
 		threshold(skinGray, binary, 200, 255, THRESH_BINARY);
 
@@ -68,24 +76,35 @@ int main()
 		int radius = 40;
 		findCenterMass(noiseCanceled, cx, cy);
 		circle(noiseCanceled, Point(cx, cy), radius, Scalar(128, 128, 128), 2);
-		circle(cameraRaw, Point(cx, cy), radius, Scalar(130, 255, 100), 2);
+		circle(camera, Point(cx, cy), radius, Scalar(130, 255, 100), 2);
 
 		Mat temp = skinGray.clone();
 		circle(temp, Point(cx, cy), (int)(radius + 0.5), Scalar(0, 0, 0), -1);
 		Mat sub = skinGray - temp;
 
+		imshow("Step 4.", sub);
+
+		// Step 5.
 		Mat edge;
 		Canny(sub, edge, 40, 110);
 		circle(edge, Point(cx, cy), radius, Scalar(0, 0, 0), 4);
 
-
-		imshow("cam", masked);
-		imshow("edge", edge);
-		imshow("preview", noiseCanceled);
-
+		imshow("Step 5.", edge);
 
 		// Enter 키를 누르면 while 빠져나가기
-		if (waitKey(1) == 13)
+		if (waitKey(1) == 13) {
+			imwrite("./outputs/step1.png", camera);
+			imwrite("./outputs/masked.png", masked);
+			imwrite("./outputs/converted.png", converted);
+			imwrite("./outputs/step2.png", skin);
+			imwrite("./outputs/graymasked.png", grayMasked);
+			imwrite("./outputs/step3.png", skinGray);
+			imwrite("./outputs/binary.png", binary);
+			imwrite("./outputs/noiseCanceled.png", noiseCanceled);
+			imwrite("./outputs/step4.png", sub);
+			imwrite("./outputs/step5.png", edge);
+
 			break;
+		}
 	}
 }
